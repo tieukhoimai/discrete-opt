c14a6214324bb728f380882948c8d824a30ef159
Mai TIEU Khoi <57632496+tieukhoimai@users.noreply.github.com>
2024-05-02 18:35:52 +0200

    feat: add the function to calculate number of feasible solutions

--- prima_PD_su_linea/prima_PD_su_linea-template.py
+++ prima_PD_su_linea/prima_PD_su_linea-template.py
@@ -1,58 +1,61 @@
+## VR505019 - KHOI MAI TIEU
+## VR501305 - PEDRO ALONSO LOPEZ TORRES
+
 #!/usr/bin/env python3
 from sys import stderr, stdout, argv
 
+MOD = 10**9 + 7
+
+supporto = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4
+}
 
 MOD = 10**9 + 7
+
+
+supporto = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4
+}
 
 def num_feasible_solutions(n):
     """returns the number of feasible solutions for an input array A of n cells."""
     assert n >= 0
-    # TODO: write here your solution! 
-    # what follows will be just good enough to respect the intended communication protocol with the server
-    # @Pedro
 
-    risp = 1 % MOD
-    return risp
+    if n in supporto:
+        return supporto[n] % MOD
 
+    tot = n + 1
 
-def solve(n, arr):
-    dp = [-1 for _ in range(n)]
-    idx = [[] for _ in range(n)]
+    # sum_{i=1}^{n-3} i = casi(casi + 1) / 2
+    casi = n - 3
+    somma = int((casi * (casi + 1)) / 2)
 
-    dp[0] = arr[0]
-    idx[0] = [0]
+    tot += somma
 
-    for i in range(1, n):
-        pick = arr[i]
+    # trovo i sottoproblemi j
+    j = 4
+    cont = 1
+    while (j * 2) - cont <= n:
+        tot += num_feasible_solutions(j) - (j + 1)
+        j += 1
+        cont += 1
 
-        # Check if there are at least three elements before the current element
-        if i > 2:
-            pick += dp[i - 3]
+    if n not in supporto:
+        supporto[n] = tot % MOD
 
-        non_pick = dp[i - 1]
-        
-        # Store the maximum of the two choices in the DP table
-        dp[i] = max(pick, non_pick)
+    return supporto[n]
 
-        if pick > non_pick:
-            idx[i].append(i)
-            if i > 2:
-                idx[i].extend(idx[i - 3])
-        else:
-            idx[i].extend(idx[i - 1])
-
-    optimal_indices = [i for i, x in enumerate(dp) if x == dp[n-1]]
-    optimal_subset = [idx[i] for i in optimal_indices]
-    optimal_solution = unique_count = len(set([tuple(sublist) for sublist in optimal_subset]))
-
-    return dp[n - 1], idx[n - 1], optimal_solution
 
 def optimize(n,A):
     """returns the triple (optval,optsol,num_optsols) where optsol is the list of indexes of any optimal solution for the instance comprising of the first n cells of array A. A first inefficient but essential solution might take inspiration from a minimal recursive implementation of the function num_feasible_solutions above"""
     assert n >= 0
     assert n == len(A)
-    # TODO: write here your solution!
-    # what follows will be just good enough to respect the intended communication protocol with the server
 
     num_optsols = 1 % MOD
 
